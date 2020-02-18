cb1ada65c069fb080819f0853382b8226b517948
AlexeyBaryliuk <alexeybaryliuk@gmail.com>
2020-02-18 10:22:01 +0300

    Try

--- department-management-test/target/classes/create-db.sql
+++
@@ -1,7 +0,0 @@
-
-DROP TABLE IF EXISTS department;
-CREATE TABLE department (
-  departmentId INT NOT NULL AUTO_INCREMENT,
-  departmentName VARCHAR(255) NOT NULL UNIQUE,
-  PRIMARY KEY (departmentId)
-);
